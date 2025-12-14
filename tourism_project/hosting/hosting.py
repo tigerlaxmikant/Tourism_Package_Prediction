8cf88badb371de2fb372cc17bcd662e4173d74d6
Laxmikant Deshpande <tigerlaxmikant@outlook.com>
2025-12-14 16:41:47 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOURISM_PACKAGE_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="/laxmikantdeshpande/tourism-package-prediction",          # the target repo
+    repo_id="laxmikantdeshpande/tourism-package-prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
